81e2fb5f889b0fa1b23b612942ca803f1992e341
Tate Ray <tate@tateray.com>
2023-12-18 12:08:58 -0700

    ...;

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ async function fetchJson()
     const response = await fetch("./assets/JSON/Text.JSON")
     const data = await response.json();
     json = await data;
-
 }
 
 async function StartUp() 
