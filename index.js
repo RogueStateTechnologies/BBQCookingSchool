d744dc95849870536c43e05c526265c87f2b9bdc
Tate Ray <tate@tateray.com>
2023-12-19 15:24:59 -0700

    ...;

---
+++ index.js
@@ -0,0 +1,41 @@
+function changeTestimonial(direction) 
+{
+    let active = document.getElementsByClassName("testimonial active")[0];
+    let activeId = Number(active.id);
+
+    if (direction === "back") 
+    {
+        console.log("moving back")
+
+        if (activeId == 1) 
+        {
+            activeId = 9;    
+        } else
+        {
+            activeId -= 1;
+        }
+
+        active.className = "testimonial inactive";
+        active = document.getElementById(activeId);
+        active.className = "testimonial active";    
+    }
+
+    if (direction === "forward")
+    {
+        console.log(activeId)
+        console.log("moving forward")
+
+        if (activeId == 9) 
+        {
+            activeId = 1;
+        }else
+        {
+            activeId += 1;
+        }
+
+        console.log(activeId)
+        active.className = "testimonial inactive";
+        active = document.getElementById(activeId);
+        active.className = "testimonial active";
+    }
+}
